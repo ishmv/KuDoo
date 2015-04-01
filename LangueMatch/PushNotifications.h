b403fe9c4d94fdf7d20e676c4cbdd6b77476603e
Travis Buttaccio <buttacciot@gmail.com>
2015-04-01 15:50:28 -0500

    replace home screen

---
+++ LangueMatch/PushNotifications.h
@@ -0,0 +1,13 @@
+//
+//  PushNotifications.h
+//  LangueMatch
+//
+//  Created by Travis Buttaccio on 3/30/15.
+//  Copyright (c) 2015 LangueMatch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PushNotifications : NSObject
+
+@end
---
+++ LangueMatch/PushNotifications.m
@@ -0,0 +1,13 @@
+//
+//  PushNotifications.m
+//  LangueMatch
+//
+//  Created by Travis Buttaccio on 3/30/15.
+//  Copyright (c) 2015 LangueMatch. All rights reserved.
+//
+
+#import "PushNotifications.h"
+
+@implementation PushNotifications
+
+@end
